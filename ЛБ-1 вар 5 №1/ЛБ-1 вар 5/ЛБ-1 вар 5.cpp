fe9df1c3d2235cbf4378319ff6079c262b53c8ee
Admin <isupovilya1711@yandex.ru>
2022-09-11 16:20:47 +0300

    1

---
+++ ЛБ-1 вар 5 №1/ЛБ-1 вар 5/ЛБ-1 вар 5.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+void Value(int x, int y)
+{
+    std::cout << "Значение=";
+    std::cout << (3 + exp(y - 1)) / (1 + pow(x, 2) * abs(y - tan(x))) << std::endl; //выражение 
+}
+
+int main()
+{
+    int x;
+    int y;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите число x=" << std::endl; //Ввод числа x
+    std::cin >> x;
+    std::cout << "Введите число y=" << std::endl; //Ввод числа y
+    std::cin >> y;
+    Value(x,y); //вызов функции value
+    return 0;
+}
---
+++ ЛР-1 вар 5 №2/ЛР-1 вар 5 №2/ЛР-1 вар 5 №2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+void func1(int x)
+{
+	std::cout << (-3 * x) + 9 << std::endl;
+}
+
+void func2(int x)
+{
+	std::cout << (1) / (x - 7) << std::endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x;
+	std::cout << "Введите число x=";
+	std::cin >> x;
+	if (x <= 7)
+	{
+		func1(x);
+	}
+	if (x > 7)
+	{
+		func2(x);
+	}
+	return 0;
+}
